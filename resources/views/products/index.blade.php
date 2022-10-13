57a21c3cb9a5efe3352f6cb48b0b1e0f34bf22d9
Faruk-web <omarfaruk6780@gmail.com>
2022-10-13 22:22:32 +0600

    gfsdgfhdggf

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,64 @@
+@extends('products.layout')
+ 
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Laravel 8 CRUD Example from scratch - ItSolutionStuff.com</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Details</th>
+            <!-- <th>Action</th> -->
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->detail }}</td>
+            <!-- <td>
+            @if($product->status == 1)
+            <a  href="{{ route('products.CatDeactive',$product->id]) }}"
+                class="btn btn-success" title="Product Active Now">Active </a>
+            @else
+
+            <a href="{{ route('products.CatActive',$product->id]) }}"
+                class="btn btn-danger" title="Product Active Now">Deactive </a>
+            @endif
+            </td> -->
+            <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+   
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+    
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+    {!! $products->links() !!}
+      
+@endsection
+
+       
